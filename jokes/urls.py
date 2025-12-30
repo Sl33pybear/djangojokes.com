d1dcd6dd7e2f89d7d53dfa32ddc6043188ddc46b
Sl33pybear <bradwebdesign101@gmail.com>
2025-12-29 19:34:02 -0500

    Initial project

--- jokes/urls.py
+++ jokes/urls.py
@@ -4,6 +4,6 @@ from .views import JokeDetailView, JokeListView
 
 app_name = 'jokes'
 urlpatterns = [
-   path('joke/<int:pk>/', JokeDetailView.as_view(), name='detail'),
-   path('', JokeListView.as_view(), name='list'),
+    path('joke/<int:pk>/', JokeDetailView.as_view(), name='detail'),
+    path('', JokeListView.as_view(), name='list'),
 ]
