5065bda3f1637d7ea3e455ccf07a9f9f26f955a8
Sl33pybear <bradwebdesign101@gmail.com>
2025-12-29 19:39:39 -0500

    Initial project

--- jokes/models.py
+++ jokes/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
 class Joke(models.Model):
     question = models.TextField(max_length=200)
     answer = models.TextField(max_length=100, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
     def get_absolute_url(self):
         return reverse('jokes:detail', args=[str(self.pk)])
 
